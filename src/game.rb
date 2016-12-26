f56ee830b6ee8a47c5c90f9a9b06a3422ddcf546
Pirolf <yuchengtu.uwm@gmail.com>
2016-12-25 19:12:10 -0800

    use poop sprite

--- src/game.rb
+++ src/game.rb
@@ -1,43 +1,53 @@
 require 'chingu'
 require './src/grid_factory'
-# also look at https://github.com/ippa/chingu/blob/master/examples/game_of_life.rb
+require './src/cell'
 
 class Game < Chingu::Window
   def initialize(pattern = nil)
     super(640,480,false)
     push_game_state(GameOfLife)
   end
-
-  def draw
-    fill_rect([0,0,640,480], 0xffffffff, -2)
-    super
-  end
 end
 
 class GameOfLife < Chingu::GameState
   traits :timer
-  CELL_SIZE = 10
+  CELL_SIZE = 23
 
 private
-  attr_accessor :grid, :grid_factory, :start_time
+  attr_accessor :grid, :grid_factory, :grid_cells
 
 public
   def initialize
     super
-    @start_time = Time.now.to_f
+
+    @grid_cells = {}
     @grid_factory = GridFactory.new
     @grid = @grid_factory.create_grid(pattern: [[1, 2], [2, 2], [3, 2]], dims: [5, 5])
-    every(1000) { @grid = @grid_factory.next_grid(@grid) }
-  end
 
-  def draw
-    @grid.cells.each_with_index do |row, i|
-      row.each_with_index do |c, j|
-        if c.alive
-          $window.fill_rect([i*CELL_SIZE,j*CELL_SIZE,CELL_SIZE,CELL_SIZE],0xff000000,0)
+    every(1000) do
+      @grid = @grid_factory.next_grid(@grid)
+
+      @grid.cells.each_with_index do |row, i|
+        row.each_with_index do |c, j|
+          key = "r#{i}c#{j}".to_sym
+          if c.alive
+            @grid_cells[key] ||= GridCell.create(x: (i + 0.5)*CELL_SIZE, y: (j + 0.5)*CELL_SIZE)
+            @grid_cells[key].show!
+          else
+            @grid_cells[key].hide! if @grid_cells.has_key? key
+          end
         end
       end
     end
+  end
+end
+
+class GridCell < Chingu::GameObject
+  trait :sprite
+
+  def setup
+    super
+    self.image = "poop.png"
   end
 end
 
